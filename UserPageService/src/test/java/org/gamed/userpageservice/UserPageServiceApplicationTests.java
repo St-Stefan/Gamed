dc4a0856732b7859b9451d4ee161fd2bf97cd18b
Matei <m.t.ivan-1@student.tudelft.nl>
2024-10-10 15:17:25 +0200

    Added microservices.

---
+++ UserPageService/src/test/java/org/gamed/userpageservice/UserPageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gamed.userpageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserPageServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
