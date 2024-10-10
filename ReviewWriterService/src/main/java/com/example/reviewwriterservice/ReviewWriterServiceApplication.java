e3f2e733545e23f181209ce16e7a45f604c03318
Matei <m.t.ivan-1@student.tudelft.nl>
2024-10-10 15:17:25 +0200

    Added microservices.

---
+++ ReviewWriterService/src/main/java/com/example/reviewwriterservice/ReviewWriterServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.reviewwriterservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewWriterServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReviewWriterServiceApplication.class, args);
+    }
+
+}
