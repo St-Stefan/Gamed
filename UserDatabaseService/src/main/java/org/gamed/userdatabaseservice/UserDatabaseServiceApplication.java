78a82e5d73becf5062ee3cd31d63068d48414521
Andrei Ionescu <a.ionescu-5@student.tudelft.nl>
2024-10-14 00:48:29 +0200

    Set up database Docker infrastructure

---
+++ UserDatabaseService/src/main/java/org/gamed/userdatabaseservice/UserDatabaseServiceApplication.java
@@ -0,0 +1,13 @@
+package org.gamed.userdatabaseservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserDatabaseServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserDatabaseServiceApplication.class, args);
+    }
+
+}
