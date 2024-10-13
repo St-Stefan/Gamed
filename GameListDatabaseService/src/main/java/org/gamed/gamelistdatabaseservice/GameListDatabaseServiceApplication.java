41b013cc330ebf342a46e2aa85cfb002388c367f
Andrei Ionescu <a.ionescu-5@student.tudelft.nl>
2024-10-14 00:48:29 +0200

    Set up database Docker infrastructure

---
+++ GameListDatabaseService/src/main/java/org/gamed/gamelistdatabaseservice/GameListDatabaseServiceApplication.java
@@ -0,0 +1,13 @@
+package org.gamed.gamelistdatabaseserviceapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameListDatabaseServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameListDatabaseServiceApplication.class, args);
+    }
+
+}
