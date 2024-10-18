7671c07c987bcca25750b5b3ac2e93ac5c91b49b
Stefan Stoicescu <stoicescu@student.tudelft.nl>
2024-10-18 19:02:27 +0200

    Added card arguments

--- frontendservice/src/components/ListCard.vue
+++ frontendservice/src/components/ListCard.vue
@@ -1,23 +1,46 @@
 <script setup>
+import { defineProps } from 'vue';
 
+defineProps({
+  username: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  imageSrc: {
+    type: String,
+    required: false,
+    default: '',
+  },
+  imageAlt: {
+    type: String,
+    required: false,
+    default: '',
+  },
+});
 </script>
 
 <template>
-  <div class="card bg-base-100 w-2/3 border border border-gray-700">
-    <figure class="h-28">
+  <div class="card bg-base-200 w-2/3 border border-gray-700 backdrop-blur-xl">
+    <figure v-if="imageSrc" class="h-28">
       <img
           src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
           alt="Shoes" />
     </figure>
     <div class="card-body">
-      <h2 class="card-title">Shoes!</h2>
-      <p>If a dog chews shoes whose shoes does he choose?</p>
+      <h2 class="card-title">{{username}}</h2>
+      <p>{{ text }}</p>
       <div class="card-actions justify-end">
-        <button class="btn btn-primary">Buy Now</button>
+        <button class="btn bg-gray-600 backdrop-blur-xl">Buy Now</button>
       </div>
     </div>
   </div>
 </template>
+
+
 
 <style scoped>
 
--- frontendservice/src/components/home/HomePage.vue
+++ frontendservice/src/components/home/HomePage.vue
@@ -2,20 +2,22 @@
 
 import WelcomeItem from "@/components/TopBar.vue";
 import SampleCard from "@/components/ListCard.vue";
+import TopBar from "@/components/TopBar.vue";
+import ListCard from "@/components/ListCard.vue";
 </script>
 
 <template>
   <div class="flex flex-col h-screen">
     <div class="flex-1 overflow-y-auto">
       <div class="sticky top-0 z-10 backdrop-blur-xl drop-shadow-xl bg-base-200/70 border-b border-gray-700">
-        <welcome-item />
+        <top-bar />
       </div>
       <div class="flex pl-20 pr-20 bg-base-200">
         <div class="w-2/3 flex-none grid place-items-center p-10 gap-10">
-          <SampleCard />
-          <SampleCard />
-          <SampleCard />
-          <SampleCard />
+          <ListCard text="Lorem ipsum" username="Test" />
+          <ListCard text="Lorem ipsum" username="Test" />
+          <ListCard text="Lorem ipsum" username="Test" />
+          <ListCard text="Lorem ipsum" username="Test" />
         </div>
 
         <div class="divider divider-horizontal"></div>
